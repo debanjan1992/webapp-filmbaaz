f157473b5c80b824611857f65f8d64d1c2c1c13f
debanjan1992 <debanjansaha1992@gmail.com>
2018-03-27 23:22:32 +0530

    'Done'

--- src/app/app-config.js
+++ src/app/app-config.js
@@ -1,3 +1,16 @@
 app.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider){
     $locationProvider.hashPrefix('').html5Mode(true);
+    $stateProvider.state('home', {
+        url: '/',
+        template: '<h1>You are at home page</h1>'
+    }).state('login', {
+        url: '/login',
+        template: '<h1>You are at login page</h1>'
+    }).state('join', {
+        url: '/join',
+        template: '<h1>You are at the registration page</h1>'
+    }).state('otherwise', {
+        url: '/error',
+        template: '<h1>Wrong route!</h1>'
+    });
 }]);
